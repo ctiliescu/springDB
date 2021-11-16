16b8a44e53b4bef4aa0eb87057fb49e70ca566bc
BudacaB <budaca.b@gmail.com>
2021-11-16 19:38:56 +0200

    return 201

--- src/main/java/com/mobile/academy/db/hello/HelloRestController.java
+++ src/main/java/com/mobile/academy/db/hello/HelloRestController.java
@@ -1,14 +1,16 @@
 package com.mobile.academy.db.hello;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRestController {
     @GetMapping("/hello")
-    public ObjectResponse helloWorld(){
+    public ResponseEntity<ObjectResponse> helloWorld(){
         System.out.println("Hello World!");
-        return new ObjectResponse("HelloWorld", "Ioan");
+        return new ResponseEntity<>(new ObjectResponse("HelloWorld", "Ioan"), HttpStatus.CREATED);
     }
 
 }
