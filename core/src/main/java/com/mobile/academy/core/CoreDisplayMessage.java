006aa662bf272150648a5ad77d4784ac08a8a2c7
Luiza <luiza.tudorache@gmail.com>
2021-11-22 09:09:58 +0200

    db connexion

--- core/src/main/java/com/mobile/academy/core/CoreDisplayMessage.java
+++ core/src/main/java/com/mobile/academy/core/CoreDisplayMessage.java
@@ -1,5 +1,8 @@
 package com.mobile.academy.core;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CoreDisplayMessage {
     public void display(String message) {
         System.out.println("logging the message ----> " + message);
--- users/src/main/java/com/mobile/academy/users/DbApplication.java
+++ users/src/main/java/com/mobile/academy/users/DbApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {
+		"com.mobile.academy.core",
+		"com.mobile.academy.users"
+})
+
 @SpringBootApplication
 public class DbApplication {
 	public static void main(String[] args) {
