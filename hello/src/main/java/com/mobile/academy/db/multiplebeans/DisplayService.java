6cdb0c2daa08626da6d688938dfb513bb56a73ed
Cristian Iliescu <cristian.iliescu@myraindrop.co.uk>
2021-11-24 08:02:28 +0200

    clean project

--- hello/src/main/java/com/mobile/academy/db/multiplebeans/DisplayService.java
+++
@@ -1,9 +0,0 @@
-package com.mobile.academy.db.multiplebeans;
-
-import com.mobile.academy.db.hello.ObjectResponse;
-
-public interface DisplayService {
-    public String getName();
-
-    public String prettyPrint(ObjectResponse objectResponse);
-}
